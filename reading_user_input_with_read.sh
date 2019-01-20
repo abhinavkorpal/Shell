a386388a75120281a7d82c026b90ae049ee484ab
Korpal, Abhinav <abhinav.korpal@accenture.com>
2019-01-20 22:46:35 +0530

    Shell

---
+++ reading_user_input_with_read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Please, enter your name
+read NAME 
+echo "Hi $NAME!"
+
+echo Please, enter your firstname and lastname
+read FN LN 
+echo "Hi $FN $LN"
---
+++ user_interfaces_select_to_make_simple_menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+OPTIONS="Hello Quit"
+select opt in $OPTIONS; do
+    if [ "$opt" = "Quit" ]; then
+     echo done
+    elif [ "$opt" = "Hello" ]; then
+     echo Hello World
+    else
+     clear
+     echo bad option
+    fi
+done
